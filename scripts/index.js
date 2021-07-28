934321774ab2842eab019501902c6af2fb2a3579
Shahriyor <shahriyordeveloping@gmail.com>
2021-07-28 14:24:14 +0300

    initial

---
+++ scripts/index.js
@@ -0,0 +1,69 @@
+$(function() {
+  $('.ozon__item-with-img').click(function() {
+    $('.ozon__item-with-img').not(this).removeClass('current')
+    $(this).toggleClass('current')
+  })
+})
+
+$(function() {
+  $('.ozon__item').click(function() {
+    $('.ozon__item').not(this).removeClass('current')
+    $(this).toggleClass('current')
+  })
+})
+
+
+function dropdown(val){
+  var y = document.getElementsByClassName('yourdrop');
+  var aNode = y[0].innerHTML=val;
+}
+
+function dropdown1(val){
+  var y = document.getElementsByClassName('yourdrop1');
+  var aNode = y[0].innerHTML=val;
+}
+
+function dropdown2(val){
+  var y = document.getElementsByClassName('yourdrop2');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown3(val){
+  var y = document.getElementsByClassName('yourdrop3');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown4(val){
+  var y = document.getElementsByClassName('yourdrop4');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown5(val){
+  var y = document.getElementsByClassName('yourdrop5');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown6(val){
+  var y = document.getElementsByClassName('yourdrop6');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown6(val){
+  var y = document.getElementsByClassName('yourdrop6');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown7(val){
+  var y = document.getElementsByClassName('yourdrop7');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown8(val){
+  var y = document.getElementsByClassName('yourdrop8');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown9(val){
+  var y = document.getElementsByClassName('yourdrop9');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown10(val){
+  var y = document.getElementsByClassName('yourdrop10');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown11(val){
+  var y = document.getElementsByClassName('yourdrop11');
+  var aNode = y[0].innerHTML=val;
+}
